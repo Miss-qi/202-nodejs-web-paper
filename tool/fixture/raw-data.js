ff72b0216a665378f4958fadf6a0dde14f0b5b93
Miss-qi <1370322806@qq.com>
2017-02-20 23:20:05 +0800

    update raw-data

--- tool/fixture/raw-data.js
+++ tool/fixture/raw-data.js
@@ -1,6 +1,6 @@
 module.exports = {
   Homework: [{
-    '_id': '58aafe860aa4eb5f06ce151c',
+      '_id': '58aafe860aa4eb5f06ce151c',
     homeworkName: 'guessNumber'
   }, {
     '_id': '58aafe980aa4eb5f06ce151d',
@@ -10,15 +10,15 @@ module.exports = {
     '_id': '58aa9523d20cf53090244b88',
     sectionName: 'java',
     homeworks: [
-      '58aa8fcf1082ff2cde000941',
-      '58aa8e019a4fc32bc8c688ee'
+      '58aafe860aa4eb5f06ce151c',
+      '58aafe980aa4eb5f06ce151d'
     ]
   },
     {
       '_id': '58aa968a3e73d03206921aee',
       sectionName: 'js',
       homeworks: [
-        '58aa8e019a4fc32bc8c688ee'
+        '58aafe980aa4eb5f06ce151d'
       ]
     }],
   Paper: [{
